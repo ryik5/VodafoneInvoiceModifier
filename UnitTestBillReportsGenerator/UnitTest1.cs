2fecbd94ea2b799f2b4a591e440e017e13f7a116
ryik5 <ryik.yuri@gmail.com>
2019-10-21 23:46:30 +0300

    wtite test

---
+++ UnitTestBillReportsGenerator/UnitTest1.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using BillReportsGenerator;
+
+namespace UnitTestBillReportsGenerator
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        { 
+        }
+    }
+}
