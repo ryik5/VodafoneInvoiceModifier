3150b8006a04e8de4e14e9e19c13c63e625cd9e9
ryik5 <ryik.yuri@gmail.com>
2019-10-22 15:11:31 +0300

    first release

--- VodafoneInvoiceModifier/Program.cs
+++ VodafoneInvoiceModifier/Program.cs
@@ -3,6 +3,9 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using Microsoft.AppCenter;
+using Microsoft.AppCenter.Analytics;
+using Microsoft.AppCenter.Crashes;
 
 namespace BillReportsGenerator
 {
@@ -16,6 +19,7 @@ namespace BillReportsGenerator
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
+            AppCenter.Start("5e211d7f-e390-491c-af88-447b25ed227d", typeof(Analytics), typeof(Crashes));
             Application.Run(new Form1());
         }
     }
