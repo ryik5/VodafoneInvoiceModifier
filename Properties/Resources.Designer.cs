620ce5f93ef81421ec6bc9fa715020018559a8e4
ryik5 <ryik.yuri@gmail.com>
2019-12-20 17:14:23 +0200

    refactoring

--- Properties/Resources.Designer.cs
+++ Properties/Resources.Designer.cs
@@ -8,7 +8,7 @@
 // </auto-generated>
 //------------------------------------------------------------------------------
 
-namespace BillReportsGenerator.Properties {
+namespace MobileNumbersDetailizationReportGenerator.Properties {
     using System;
     
     
@@ -39,7 +39,7 @@ namespace BillReportsGenerator.Properties {
         internal static global::System.Resources.ResourceManager ResourceManager {
             get {
                 if (object.ReferenceEquals(resourceMan, null)) {
-                    global::System.Resources.ResourceManager temp = new global::System.Resources.ResourceManager("BillReportsGenerator.Properties.Resources", typeof(Resources).Assembly);
+                    global::System.Resources.ResourceManager temp = new global::System.Resources.ResourceManager("MobileNumbersDetailizationReportGenerator.Properties.Resources", typeof(Resources).Assembly);
                     resourceMan = temp;
                 }
                 return resourceMan;
