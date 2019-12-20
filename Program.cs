021519e996d234f9206417d723190b7bb606b827
ryik5 <ryik.yuri@gmail.com>
2019-12-20 17:53:39 +0200

    refactoring

--- Program.cs
+++ Program.cs
@@ -3,9 +3,6 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using System.Windows.Forms;
-using Microsoft.AppCenter;
-using Microsoft.AppCenter.Analytics;
-using Microsoft.AppCenter.Crashes;
 
 namespace MobileNumbersDetailizationReportGenerator
 {
@@ -19,7 +16,6 @@ namespace MobileNumbersDetailizationReportGenerator
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            AppCenter.Start("5e211d7f-e390-491c-af88-447b25ed227d", typeof(Analytics), typeof(Crashes));
             Application.Run(new Form1());
         }
     }
