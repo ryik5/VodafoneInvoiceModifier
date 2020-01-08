9ecf266dd3808739234140a6d4497ec579f987ca
ryik5 <ryik.yuri@gmail.com>
2020-01-08 23:55:47 +0200

    refactoring

---
+++ Classes/ListExtensions.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MobileNumbersDetailizationReportGenerator
+{
+   public static class ListExtensions
+    {
+        public static void WriteAtFile(this List<string> source, string filePath)
+        {
+            using (StreamWriter swExtLogFile = new StreamWriter(filePath, true))
+            {
+                foreach (var s in source.ToArray())
+                {
+                    swExtLogFile.WriteLine(s);
+                }
+            }
+        }
+
+        public static void AppendAtFile(this List<string> source, string filePath)
+        {
+            File.AppendAllLines(
+                filePath,
+                source,
+                Encoding.GetEncoding(1251));
+        }
+
+        public static string AsString(this List<string> source, string separator)
+        {
+            return string.Join(separator, source.ToArray());
+        }
+
+        //public static void WriteAtFile(this List<string> listStrings, string filePath)
+        //{
+        //    File.WriteAllLines(
+        //        filePath,
+        //        listStrings,
+        //        Encoding.GetEncoding(1251));
+        //}
+        
+    }
+}
--- Classes/WinFormsExtensions.cs
+++ Classes/WinFormsExtensions.cs
@@ -8,93 +8,53 @@ using System.Windows.Forms;
 
 namespace MobileNumbersDetailizationReportGenerator
 {
-    internal static class WinFormsExtensions
+    public static class WinFormsExtensions
     {
-        internal static void AppendLine(this TextBox source, string value = "\r\n")
+        public static void AppendLine(this TextBox source, string value = "\r\n")
         {
             if (source?.Text?.Length == 0)
                 source.Text = value;
             else
-                source.AppendText("\r\n" + value);
+                source.AppendText($"{Environment.NewLine} {value}");
         }
+        
 
-        internal static void WriteAtFile(this string source, string filePath)
-        {
-            File.WriteAllText(
-                filePath,
-                source,
-                Encoding.GetEncoding(1251));
-        }
-
-        internal static void WriteAtFile(this List<string> source, string filePath)
-        {
-            using (StreamWriter swExtLogFile = new StreamWriter(filePath, true))
-            {
-                foreach (var s in source.ToArray())
-                {
-                    swExtLogFile.WriteLine(s);
-                }
-            }
-        }
-
-        internal static void AppendAtFile(this string source, string filePath)
-        {
-            File.AppendAllText(
-                filePath,
-                source,
-                Encoding.GetEncoding(1251));
-        }
-
-        //internal static void WriteAtFile(this List<string> listStrings, string filePath)
-        //{
-        //    File.WriteAllLines(
-        //        filePath,
-        //        listStrings,
-        //        Encoding.GetEncoding(1251));
-        //}
-        internal static void AppendAtFile(this List<string> listStrings, string filePath)
-        {
-            File.AppendAllLines(
-                filePath,
-                listStrings,
-                Encoding.GetEncoding(1251));
-        }
-
-        internal static string AsString(this List<string> source, string separator)
-        {
-            return string.Join(separator, source.ToArray());
-        }
-
-        internal static decimal TryParseAsInternetTrafic(this string source, string formResult)
+        /// <summary>
+        /// waiting string as '200 Mb'
+        /// </summary>
+        /// <param name="source"></param>
+        /// <param name="formResult"></param>
+        /// <returns></returns>
+        public static decimal ToInternetTrafic(this string source, string formResult)
         {
 
-            string endSource = source?.Trim()?.ToUpper();
-           
-            if (endSource?.Length < 1|| !endSource.Contains(' '))
+            string text = source?.Trim()?.ToUpper();
+
+            if (text?.Length < 1 || !text.Contains(' '))
                 return 0;
 
             string end = string.Empty;
 
-            if (endSource.EndsWith("MB"))
+            if (text.EndsWith("MB"))
             {
                 end = "Mb";
             }
-            else if (endSource.EndsWith("KB"))
+            else if (text.EndsWith("KB"))
             {
                 end = "Kb";
             }
-            else if (endSource.EndsWith("B"))
+            else if (text.EndsWith("B"))
             {
                 end = "b";
             }
 
-            int.TryParse(endSource?.Remove(endSource.IndexOf(' ')), out int parsed);
-            
+            int.TryParse(text?.Remove(text.IndexOf(' ')), out int parsed);
+
             decimal result;
             switch (end)
             {
                 case ("Mb"):
-                    result = parsed * 1024 * 1024/MultiplierInternetTrafic.MultiplierInB(formResult);
+                    result = parsed * 1024 * 1024 / MultiplierInternetTrafic.MultiplierInB(formResult);
                     break;
                 case ("Kb"):
                     result = parsed * 1024 / MultiplierInternetTrafic.MultiplierInB(formResult);
@@ -109,6 +69,6 @@ namespace MobileNumbersDetailizationReportGenerator
 
             return Math.Round(result, 2);
         }
-    }
 
+    }
 }
