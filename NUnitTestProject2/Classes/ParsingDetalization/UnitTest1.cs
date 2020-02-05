328bbc38e8f598930aaae775ed22938b054e941f
ryik5 <ryik.yuri@gmail.com>
2020-02-06 00:15:22 +0200

    refactoring

---
+++ NUnitTestProject2/Classes/ParsingDetalization/UnitTest1.cs
@@ -0,0 +1,31 @@
+﻿using NUnit.Framework;
+using MobileNumbersDetailizationReportGenerator;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MobileNumbersDetailizationReportGenerator
+{
+    [TestFixture()]
+    public class UnitTest1
+    {
+        [Test()]
+        public void SplitWholeBillToSeparatedContractsTest()
+        {
+          //  Assert.Fail();
+        }
+    }
+}
+
+namespace MobileNumbersDetailizationReportGenerator.Tests
+{
+    [TestFixture()]
+    public class UnitTest1
+    {
+        [Test()]
+        public void ParserDetalizationTest()
+        {
+          //  Assert.Fail();
+        }
+    }
+}
